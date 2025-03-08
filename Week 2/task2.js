08af45750b144dacf85e6062730e62f272635f4c
Abhay Prasad <90571410+AbhayPrasad25@users.noreply.github.com>
2025-03-09 01:46:25 +0530

    Week 2

---
+++ Week 2/task2.js
@@ -0,0 +1,8 @@
+let x = 23,
+  y = 15;
+let addition = x + y;
+console.log("Addition:", addition);
+let multiplication = x * y;
+console.log("Multiplication:", multiplication);
+let modulus = x % y;
+console.log("Modulus:", modulus);
---
+++ Week 2/task3.js
@@ -0,0 +1,5 @@
+let n = 10;
+let isEven;
+if (n % 2 == 0) isEven = "Even";
+else isEven = "Odd";
+console.log("The number is:", isEven);
